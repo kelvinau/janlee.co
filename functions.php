728274111b9e13186c241160fca7eb1ff0dfe159
Kelvin Au <kelvinau4413@hotmail.com>
2018-05-06 11:21:42 -0700

    rename parent style handler

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ $PARENT_DIR = get_template_directory_uri();
 $CHILD_DIR = get_stylesheet_directory_uri();
 function child_enqueue() {
     global $PARENT_DIR, $CHILD_DIR;
-    $parent_style = 'penci-soledad-parent-style';
+    $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style , "{$PARENT_DIR}/style.css" );
     wp_enqueue_style( 'child-style', "{$CHILD_DIR}/css/custom.css" , array( $parent_style ));
 
