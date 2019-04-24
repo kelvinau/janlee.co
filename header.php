99e48a0311332387b1406eb85506704818973d75
Kelvin Au <kelvinau4413@hotmail.com>
2019-04-23 21:00:22 -0700

    add google font

--- header.php
+++ header.php
@@ -32,6 +32,7 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">  
 	<?php wp_head(); ?>
 </head>
 
