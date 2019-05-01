18b3a25aeed6f3fa6f5c12bf1147bd389a2ca1b5
Kelvin Au <kelvinau4413@hotmail.com>
2019-04-30 23:43:02 -0700

    add modified date

--- functions.php
+++ functions.php
@@ -9,3 +9,14 @@ function child_enqueue() {
     wp_enqueue_style( 'child-style', "{$CHILD_DIR}/css/custom.css" , array( $parent_style ));
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue', 999 );
+
+function cyb_add_last_modified_header($headers) {
+    //Check if we are in a single post of any type (archive pages has not modified date)
+    if( is_singular() ) {
+        $post_id = get_queried_object_id();
+        if( $post_id ) {
+            header("Last-Modified: " . get_the_modified_time("D, d M Y H:i:s", $post_id) );
+        }
+    }
+}
+add_action('template_redirect', 'cyb_add_last_modified_header');
